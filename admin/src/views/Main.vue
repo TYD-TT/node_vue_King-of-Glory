b88e60adce632892ac81a5ec781dd70576823a57
田玉东 <2285955616@qq.com>
2020-08-29 16:30:04 +0800

    基础配置

---
+++ admin/src/views/Main.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <el-container style="height:100vh">
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+      <el-container>
+        <el-aside
+          :width="isCollapse ? '64px' : '200px'"
+          style="background-color: rgb(238, 241, 246)"
+        >
+          <!-- 侧边栏的折叠和展开 -->
+          <div class="toggle-button" @click="toggleCollapse">
+            <i :class="arrows2" ref="caret"></i>
+          </div>
+          <el-menu :default-openeds="['1', '3']" :collapse="isCollapse">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-message" style="padding-right:20px;"></i>导航一
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-menu" style="padding-right:20px;"></i>导航二
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="2-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-setting" style="padding-right:20px;"></i>导航三
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="3-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="3-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="日期" width="140"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+      tableData: Array(10).fill(item),
+      //   控制侧边栏的展开与收缩
+      isCollapse: false,
+      //   控制侧边栏展开与收缩的箭头
+      arrows1: -1,
+      arrows2: "el-icon-caret-left",
+    };
+  },
+  methods: {
+    //   控制侧边栏的展开与收缩
+    toggleCollapse() {
+      this.arrows1 = -this.arrows1;
+      if (this.arrows1 > 0) {
+        this.arrows2 = "el-icon-caret-right";
+      } else {
+        this.arrows2 = "el-icon-caret-left";
+      }
+      this.isCollapse = !this.isCollapse;
+    },
+  },
+};
+</script>
+
+<style lang="less" scope>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+
+.toggle-button {
+  background-color: #eef1f6;
+  font-size: 20px;
+  line-height: 24px;
+  color: #b3c0d1;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
+
