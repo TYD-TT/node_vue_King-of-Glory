0abfaa228944ed998070ee3b944984cbba1f14cb
田玉东 <tyd2285955616@163.com>
2020-09-06 22:35:34 +0800

    前端首页

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './style.scss'
+import router from './router/router'
+
+// 轮播图片
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+ 
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/f957c0a79d7403fa93afeb59f995fb4c.jpeg" alt="" srcset="">
+      </swiper-slide>
+      <swiper-slide>22222</swiper-slide>
+      <swiper-slide>33333</swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+</style>
